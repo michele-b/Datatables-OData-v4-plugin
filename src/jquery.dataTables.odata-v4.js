57c9270c457d7aceed3ff51ff3d0063c093bb9fd
luca-vercelli <luca.vercelli.to@gmail.com>
2018-12-10 11:51:54 +0100

    Show errors dialog

--- src/jquery.dataTables.odata-v4.js
+++ src/jquery.dataTables.odata-v4.js
@@ -212,7 +212,7 @@ function ajaxOData(data, callback, settings) {
         "error": function (jqXHR, textStatus, errorThrown) {
         	settings.oApi._fnCallbackFire(settings, null, 'xhr', [settings, null, settings.jqXHR]);
         	settings.oApi._fnProcessingDisplay(settings, false);
-        	settings.oApi._fnLog(settings, 0, "Error while loading data: " + textStatus);
+        	settings.oApi._fnLog(settings, 0, "Error while loading data: " + textStatus + " - " + errorThrown);
         }
     }));
     
